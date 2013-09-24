89ef73eaff1850854d88817ad2cf4977eeb86999
Christian Henke <christian@cebitec.uni-bielefeld.de>
2013-09-24 13:57:18 +0200

    simplified a construct

--- src/de/unibi/cebitec/aws/s3/transfer/BiBiS3.java
+++ src/de/unibi/cebitec/aws/s3/transfer/BiBiS3.java
@@ -428,7 +428,7 @@ public class BiBiS3 {
                                     String relativePathString = item.getKey().substring(keyPrefix.length());
                                     log.debug("relative path string: {}", relativePathString);
                                     Path fileDestinationPath = destination;
-                                    if (destination.startsWith(".") && destination.endsWith(".")) {
+                                    if (destination.equals(".")) {
                                         fileDestinationPath = fileDestinationPath.resolve(dirname);
                                     }
                                     fileDestinationPath = fileDestinationPath.resolve(relativePathString);
